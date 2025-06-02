c8680db6363bad95f941ca54480e0de04e889685
Plattfisken <158032673+Plattfisken@users.noreply.github.com>
2025-06-02 23:41:31 +0200

    useful things

---
+++ useful_things.h
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef USEFUL_THINGS_H
+#define USEFUL_THINGS_H
+
+// common types
+#define KILOBYTES(n) ((1024) * (n))
+#define MEGABYTES(n) ((1024) * (KILOBYTES(n)))
+#define GIGABYTES(n) ((1024) * (MEGABYTES(n)))
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+// Maybe?
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+// memory management and arenas
+#define UT_ARENA_DEFAULT_SIZE KILOBYTES(64)
+
+typedef struct s_arena UT_Arena;
+struct s_arena {
+    struct s_arena *next;
+    size_t size;
+    u8 *cur;
+    u8 memory[];
+};
+
+UT_Arena *UT_arena_create_size(size_t size);
+UT_Arena *UT_arena_create();
+
+void *UT_arena_alloc(UT_Arena *arena, size_t size);
+void UT_arena_free(UT_Arena *arena);
+
+// file management
+size_t UT_get_file_size(FILE *f);
+
+#ifdef USEFUL_THINGS_IMPLEMENTATION
+UT_Arena *UT_arena_create_size(size_t size) {
+    UT_Arena *arena = malloc(sizeof(*arena) + size);
+    if(!arena) return NULL;
+    arena->next = NULL;
+    arena->size = size;
+    arena->cur = arena->memory;
+    return arena;
+}
+
+UT_Arena *UT_arena_create() {
+    u32 default_size = UT_ARENA_DEFAULT_SIZE;
+    return UT_arena_create_size(default_size);
+}
+
+void *UT_arena_alloc(UT_Arena *arena, size_t size) {
+    UT_Arena *linked_arena = arena;
+    // Loop through all the arenas in the linked list. If one has space, allocate there and return.
+    while(linked_arena) {
+        size_t space_left = (linked_arena->memory + linked_arena->size) - linked_arena->cur;
+        if(size <= space_left) {
+            void *ret = linked_arena->cur;
+            linked_arena->cur += size;
+            return ret;
+        }
+        else {
+            linked_arena = linked_arena->next;
+        }
+    }
+    // No arena had space. Create a new one and add to linked list
+    size_t new_arena_size = size > arena->size ? size : arena->size;
+    UT_Arena *new_arena = UT_arena_create_size(new_arena_size);
+
+    new_arena->next = arena->next;
+    arena->next = new_arena;
+    void *ret = new_arena->cur;
+    new_arena->cur += size;
+    return ret;
+}
+
+void UT_arena_free(UT_Arena *arena) {
+    UT_Arena *next = arena;
+    while(next->next) {
+        arena = next;
+        next = next->next;
+        free(arena);
+    }
+    free(next);
+}
+
+size_t UT_get_file_size(FILE *f) {
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    return size;
+}
+
+#endif
+#endif
